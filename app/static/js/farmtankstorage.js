ec380319572a5da412db8676decac421a12aa6e7
IoS <ianoshea10@yahoo.com>
2024-02-14 07:47:41 +0000

    fix incorrect tank calculations

--- app/static/js/farmtankstorage.js
+++ app/static/js/farmtankstorage.js
@@ -4,13 +4,13 @@ document.getElementById("tankheight").addEventListener("input", myFunction);
 
 function myFunction() {
   console.log("Start of Tank Capacity Calculation")
-  let tanklength =  document.getElementById("tankheight").value;
+  let tanklength =  parseFloat(document.getElementById("tanklength").value);
   console.log(tanklength)
-  let tankwidth =  document.getElementById("tankwidth").value;
+  let tankwidth =  parseFloat(document.getElementById("tankwidth").value);
   console.log(tankwidth)
-  let tankheight =  document.getElementById("tankheight").value;
+  let tankheight =  parseFloat(document.getElementById("tankheight").value);
   console.log(tankheight)
   console.log(tanklength * tankwidth * tankheight)
-  document.getElementById("tankcapacity").value = 1000 * tanklength * tankwidth * tankheight 
+  document.getElementById("tankcapacity").value = 1000 * (tanklength) * (tankwidth) * (tankheight) 
   document.getElementById("regulatoryfillcapacityallowed").value = 1000 * (tanklength * tankwidth * (tankheight -0.02))
 }
